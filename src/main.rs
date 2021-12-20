508ddc6a1b16ad13ac366d14772a32d6401864f4
snpefk <snpefk@gmail.com>
2021-12-20 13:08:54 +0300

    add Runner struct

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,17 @@ use termion::raw::IntoRawMode;
 use tui::backend::TermionBackend;
 use tui::Terminal;
 
+struct Runner {
+    id: usize,
+    description: String,
+    ip_address: String,
+    active: bool,
+    is_shared: bool,
+    name: String,
+    online: bool,
+    status: String,
+}
+
 fn main() -> Result<(), io::Error> {
     let stdout = io::stdout().into_raw_mode()?;
     let backend = TermionBackend::new(stdout);
