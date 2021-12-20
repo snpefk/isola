bb29270b0b76bbc9522041e960e68bf35ce2c748
snpefk <snpefk@gmail.com>
2021-12-20 13:56:25 +0300

    ui: add create_table

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::io;
 use termion::raw::IntoRawMode;
 use tui::backend::TermionBackend;
+use tui::layout::Constraint;
+use tui::style::{Color, Modifier, Style};
+use tui::widgets::{Block, Borders, Row, Table};
 use tui::Terminal;
 
 struct Runner {
@@ -21,9 +24,71 @@ fn main() -> Result<(), io::Error> {
 
     terminal.clear()?;
 
-    loop {
-        terminal.draw(|f| {})?;
-    }
+    let runners: Vec<Runner> = vec![];
+    terminal.draw(|f| {
+        f.render_widget(create_table(&runners), f.size());
+    })?;
 
     Ok(())
+}
+
+fn create_table(runners: &Vec<Runner>) -> Table {
+    let headers_style = Style::default()
+        .fg(Color::Black)
+        .add_modifier(Modifier::BOLD);
+    let headers = [
+        "ID",
+        "NAME",
+        "DESCRIPTION",
+        "IP",
+        "SHARED",
+        "ACTIVE",
+        "ONLINE",
+        "STATUS",
+    ];
+
+    let headers = Row::new(headers).style(headers_style);
+
+    let rows: Vec<Row> = runners
+        .iter()
+        .map(|r| {
+            let row = Row::new(vec![
+                r.id.to_string(),
+                r.name.to_string(),
+                r.description.to_string(),
+                r.ip_address.to_string(),
+                r.is_shared.to_string(),
+                r.active.to_string(),
+                r.online.to_string(),
+                r.status.to_string(),
+            ]);
+
+            if r.active {
+                row.style(Style::default().fg(Color::Green))
+            } else {
+                row
+            }
+        })
+        .collect();
+
+    Table::new(rows)
+        .header(headers)
+        .style(Style::default().fg(Color::White))
+        .block(
+            Block::default()
+                .borders(Borders::ALL)
+                .title(" Runners ")
+                .title_alignment(tui::layout::Alignment::Center),
+        )
+        .highlight_style(Style::default().bg(Color::LightCyan))
+        .widths(&[
+            Constraint::Length(5),
+            Constraint::Length(15),
+            Constraint::Length(40),
+            Constraint::Length(15),
+            Constraint::Length(10),
+            Constraint::Length(8),
+            Constraint::Length(8),
+            Constraint::Length(10),
+        ])
 }
